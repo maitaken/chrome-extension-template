a65092db1f8411461ef7193b927aa861a5446379
maitaken <ti0802development@gmail.com>
2023-04-09 17:53:35 +0900

    contentssciprtとの通信テンプレート追加

--- src/contents/contents.tsx
+++ src/contents/contents.tsx
@@ -1,1 +1,4 @@
-console.log("Hello from contents hoge")
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    console.log(request.message);
+    sendResponse({ message: "Hello from content script!" })
+});
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,18 +1,23 @@
 import { createRoot } from 'react-dom/client';
-import { useState } from 'react';
+
+const sendMessage = () => {
+    console.log("send message")
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(
+            tabs[0].id,
+            { message: "Hello from popup!" },
+            (resp) => {
+                console.log("get message", resp.message)
+            }
+        );
+    });
+}
 
 const PopUp = () => {
-    const [count, setCount] = useState(0)
-    const h = []
-    for (let i = 0; i < count; i++) {
-        h.push(<div>hoge</div>)
-    }
     return <div>
-        {h}
-        <button onClick={() => setCount(count + 1)}>push</button>
+        <button onClick={sendMessage}>push</button>
     </div>
 }
 
-console.log(document.getElementById("App"))
 const root = createRoot(document.getElementById("App"))
 root.render(<PopUp></PopUp>)
